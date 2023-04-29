d339284a2230718e6d4bce7c25d1d648f3ad9392
Eduardo <edu.he.dev@gmail.com>
2023-04-29 15:39:03 -0300

    css

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,24 @@
 <script lang="ts">
 </script>
 
-<main>ola mundo</main>
+<main class="main">
+  <div class="climate">ola</div>
+  <div class="search">mundo</div>
+</main>
 
 <style>
+  .main {
+    width: 100%;
+    height: 100vh;
+    outline: 1px solid red;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 100%;
+  }
+  .climate {
+    outline: 1px solid blue;
+  }
+  .search {
+    outline: 1px solid red;
+  }
 </style>
